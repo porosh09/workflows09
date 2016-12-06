467b68ab50a99dc26e22b3b69fae38a7f05c83c9
Porosh <you@example.com>
2016-12-05 18:00:58 -0800

    Add watch task

--- gulpfile.js
+++ gulpfile.js
@@ -38,9 +38,62 @@ gulp.task('compass', function(){
           .on('error',gutil.log))
 .pipe(gulp.dest('builds/development/css'))
           });
-
+gulp.task('watch',function(){
+    gulp.watch(coffeeSource, ['coffee']);
+    gulp.watch(jsSources, ['js']);
+     gulp.watch('components/sass/*.scss', ['compass']);
+});
 
 gulp.task('default',['coffee', 'js', 'compass']);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
