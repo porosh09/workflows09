947d594f491003acfe585f20620e6b7b196bbfbf
Porosh <you@example.com>
2016-12-05 20:50:33 -0800

    add Production File

--- gulpfile.js
+++ gulpfile.js
@@ -14,8 +14,21 @@ var jsSources = [
     'components/scripts/tagline.js',
     'components/scripts/template.js'
 ];
+var  env,
+    outputDir,
+    sassStyle;
+ env = process.env.NODE_ENV || 'development';
+ 
+if(env==='development'){
+    outputDir = 'builds/development/';
+    sassStyle = 'expanded';
+}else{
+     outputDir = 'builds/production/';
+    sassStyle = 'compressed';
+}
+
 var sassSources =['components/sass/style.scss']; 
-var htmlSource = ['builds/development/*.html'];
+var htmlSource = [outputDir + '*.html'];
 gulp.task('coffee', function(){
     gulp.src(coffeeSource)
     .pipe(coffee({ bare: true })
@@ -27,7 +40,7 @@ gulp.task('js', function(){
           gulp.src(jsSources)
             .pipe(concat('script.js'))
           .pipe(browserify())
-.pipe(gulp.dest('builds/development/js'))
+.pipe(gulp.dest(outputDir + 'js'))
           .pipe(connect.reload())
           });
 
@@ -35,11 +48,11 @@ gulp.task('compass', function(){
           gulp.src(sassSources)
             .pipe(compass({
               sass: 'components/sass',
-              image: 'builds/development/images',
-              style: 'expanded'
+              image: outputDir + 'images',
+              style: sassStyle
           })
           .on('error',gutil.log))
-.pipe(gulp.dest('builds/development/css'))
+.pipe(gulp.dest(outputDir + 'css'))
            .pipe(connect.reload())
           });
 gulp.task('watch',function(){
@@ -52,7 +65,7 @@ gulp.task('watch',function(){
 
 gulp.task('connect',function(){
     connect.server({
-        root: 'builds/development/',
+        root: outputDir,
         livereload: true
     });
 });
@@ -63,7 +76,7 @@ gulp.task('html', function(){
 });
 
 gulp.task('json', function(){
-    gulp.src('builds/development/js/*.json')
+    gulp.src(outputDir + 'js/*.json')
     .pipe(connect.reload())
 });
 
